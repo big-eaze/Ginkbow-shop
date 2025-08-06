5743eaa352c61471af9e3dfad41c00608c0d90c6
Israel <israelojeleye35@gmail.com>
2025-08-06 22:03:37 +0100

    fourth version

--- ginkbow-backend-2/server.js
+++ ginkbow-backend-2/server.js
@@ -31,13 +31,7 @@ app.use(cors()); // Add this line to enable CORS for all routes
 
 app.use(express.json());
 
-const PORT = process.env.PORT;
-
-if (!PORT) {
-  console.error("❌ ERROR: Render didn't provide a PORT");
-  process.exit(1);
-}
-
+const PORT = process.env.PORT || 8080;
 
 
 // Routes
